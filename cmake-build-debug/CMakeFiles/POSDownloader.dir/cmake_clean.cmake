d8951dd477c47f9a79f794089a15bd8db94973ef
PatRusi98 <patrik.rusinak@gmail.com>
2022-01-09 12:46:52 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/POSDownloader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POSDownloader.dir/main.c.o"
+  "POSDownloader"
+  "POSDownloader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POSDownloader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("                     DOWNLOAD MANAGER\n");
+    printf("------------------------------------------------------------\n");
+    printf("         created by Patrik Rusinak, Kevin Laurincik\n");
+    printf("------------------------------------------------------------\n");
+
+    return 0;
+}
